e6b23fada4b06f7434aa035b0d9eb6a6469cd1d0
JulianG91012 <juliangomez91012@gmail.com>
2022-09-11 18:36:29 -0500

    Se agregan los comentarios y se arreglan bugs

--- PensamientoComputacional/funciones_abstraccion.py
+++ PensamientoComputacional/funciones_abstraccion.py
@@ -1,23 +1,23 @@
 
+#Imprimir el menú general con las opciones válidas.
 def printMenu():
     options =""" 
     1. Raíz Cuadrada por Enumeración 
     2. Raíz Cuadrada por Aproximación
     3. Raíz Cuadrada por Búsqueda Binaria
+    4. Si desea salir del programa
     """
     print(options) 
 
+#Validar la respuesta del usuario con las opciones disponibles.
 def printOpciones():
-    while True:
+    option_selected = input()
+    while option_selected not in ["1","2","3","4"]:
+        print(f"No ha ingresado una opción válida. Intente nuevamente")
         option_selected = input()
-        if option_selected in ["1","2","3"]:
-            return option_selected
-        elif option_selected == "5":
-            break
-        else:
-            print(f"No ha ingresado una opción válida. Intente nuevamente")
-            print("Si desea salir, presione x")
+    return option_selected
 
+#Recibe la respuesta del usuario y en base a ello realiza lo solicitado
 def opcionElegida(eleccion, number):
     if eleccion == "1":
         enumeracion(number)
@@ -25,7 +25,11 @@ def opcionElegida(eleccion, number):
         aproximacion(number)
     elif eleccion == "3":
         busqueda_binaria(number)
+    if eleccion == "4":
+        print("Cerrando programa...")
+        return 1
 
+#Resolver una raíz cuadrada por el método de la Enumeración
 def enumeracion(number):
     respuesta = 0
     number = int(number)
@@ -35,13 +39,12 @@ def enumeracion(number):
         print(f'La raíz cuadrada de {number} es {respuesta}\n')
     else:
         print(f'El {number} no tiene una raiz cuadrada exacta\n')
-    # lastMenu(number)
 
+#Resolver una raíz cuadrada por el método de la aproximación
 def aproximacion(number):
     epsilon = 0.01
     number = int(number)
     respuesta = 0.0
-    print(f'Para este Método, usaremos una medida de epsilon de {epsilon} (Esto es la medida de qué tan precisos queremos ser)')
     paso = epsilon**2 #Qué tanto nos iremos acercando a la respuesta en cada iteracion
     while abs(respuesta**2 - number) >= epsilon and respuesta <= number:
         print(abs(respuesta**2 - number), respuesta)
@@ -51,12 +54,13 @@ def aproximacion(number):
         str_respuesta = print(f'No se encontro la raiz cuadrada de {number}\n')
     else:
         str_respuesta = print(f'La raiz cuadrada de {number} es {respuesta}\n')
-    # lastMenu(number)
+        print(f'Para este Método, se usó la medida epsilon de {epsilon} (Esto es la medida de qué tan precisos queremos ser)')
 
+#Resolver una raíz cuadrada por el método de la Búsqueda Binaria
 def busqueda_binaria(number):
     number = int(number)
     epsilon = 0.01
-    print(f'Para este Método, usaremos una medida de epsilon de {epsilon} (Esto es la medida de qué tan precisos queremos ser)')
+    print(f'Para este Método, se usó la medida epsilon de {epsilon} (Esto es la medida de qué tan precisos queremos ser)')
     low = 0.0
     high = max(1.0, number)#Si el objetivo es menor a 1.0 se empieza en 1.0
     result = (high + low /2)
@@ -69,21 +73,21 @@ def busqueda_binaria(number):
             high = result
         result = (high + low)/2
     print(f'La raiz cuadrada de {number} es {result}\n')
-    # lastMenu(number)
 
+#Preguntarle al usuario si desea realizar la raíz con otro método
 def lastMenu(number):
     print("¿Desea usar algún otro método?")
     print("Escriba 1 para intentar otro método")
     print("Escriba 2 Para salir del programa")
     respuesta = input()
     while respuesta not in ["1","2"]:
-        respuesta = input("Ingrese una de las opciones correctas")
+        respuesta = input("Ingrese una de las opciones correctas\n")
     if respuesta == "1":
-        print("Vamos a trabajar con el mismo número que se usó al principio.")
+        print(f"Vamos a trabajar con el número {number}, que se ingresó al principio.")
         printMenu()
         eleccion = printOpciones()
-        opcionElegida(eleccion, number)
-        lastMenu(number)
+        if opcionElegida(eleccion, number) != 1:
+            lastMenu(number)
     if respuesta == "2":
         print("Gracias, terminando programa...")
 
@@ -94,16 +98,8 @@ def run():
     print("Ahora ingresa el método con el que quieres realizar la raíz: ")
     printMenu()
     eleccion = printOpciones()
-    opcionElegida(eleccion, number)
-    lastMenu(number)
-    # respuesta = input("Que quieres hacer? 1. Para continuar, 2. para salir")
-    # while (respuesta != "2"):
-    #     if respuesta == "1":
-    #         print("Repitamos")
-    #     else:
-    #         print("Ingrese algo bueno")
-    #         respuesta = input()
-    # print("Gracias, cerrando")
+    if opcionElegida(eleccion, number) != 1:
+        lastMenu(number)
 
 
 if __name__ =="__main__":
